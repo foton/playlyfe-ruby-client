2a303e950c0f3379bd3721159087c2dba5ca96e2
Petr Mlčoch <you@example.com>
2016-04-26 14:14:38 +0200

    added minitest for testing

--- Gemfile
+++ Gemfile
@@ -1,3 +1,9 @@
 source 'https://rubygems.org'
 
 gem 'playlyfe'  #SDK for PlayLyfe API
+
+
+gem 'byebug'
+gem "pry-rails" # pry misto IRB v rails console
+gem "pry-byebug" #debugging with pry on ruby 2 ('step','next','finish','continue')
+gem "minitest-reporters" #better formatted output of Minitest
---
+++ test/test_helper.rb
@@ -0,0 +1,15 @@
+#RUN SINGLE TEST?  
+#rake test TEST=test/models/identity_test.rb TESTOPTS="--name=test_can_be_created_from_auth_without_user -v"
+
+#RUN ALL TESTS IN FILE?
+#rake test TEST=test/models/identity_test.rb 
+
+require "minitest/reporters"
+require_relative "./rake_rerun_reporter"
+require 'minitest/autorun'
+
+reporter_options = { color: true, slow_count: 5, verbose: false, rerun_prefix: "rm -f log/*.log && be" }
+#Minitest::Reporters.use! [Minitest::Reporters::DefaultReporter.new(reporter_options)]
+Minitest::Reporters.use! [Minitest::Reporters::RakeRerunReporter.new(reporter_options)]
+
+
